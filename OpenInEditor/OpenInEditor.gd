8d17843c1c67764010ed52661ff6e6c81e76e9a1
Creta Park <creta5164@gmail.com>
2024-05-14 23:54:05 +0900

    style: Clean up code readability.

--- OpenInEditor/OpenInEditor.gd
+++ OpenInEditor/OpenInEditor.gd
@@ -1,26 +1,30 @@
 @tool
-extends EditorPlugin
+class_name OpenInEditor extends EditorPlugin
 
-static var POPUP_MENU_ID = 5164
-static var NODE_PATH_CONTEXT_MENU = "@PopupMenu@5811"
+const POPUP_MENU_ID          := 5164
+const NODE_PATH_CONTEXT_MENU := "@PopupMenu@5811"
 
-var file_system_dock_context_menu:PopupMenu
-
+var file_system_dock_context_menu: PopupMenu
 
 func _enter_tree() -> void:
-	var file_system_dock = EditorInterface.get_file_system_dock();
+	
+	var file_system_dock := EditorInterface.get_file_system_dock();
 	
 	if !is_instance_valid(file_system_dock_context_menu):
+		
 		file_system_dock_context_menu = file_system_dock.get_node(NODE_PATH_CONTEXT_MENU)
 		
 		if !is_instance_valid(file_system_dock_context_menu):
-			return;
+			return
+	
 	file_system_dock_context_menu.about_to_popup.connect(popup_menu_about_to_pop_up)
 	file_system_dock_context_menu.id_pressed.connect(popup_menu_id_pressed)
 
 
 func _exit_tree() -> void:
+	
 	if is_instance_valid(file_system_dock_context_menu):
+		
 		file_system_dock_context_menu.about_to_popup.disconnect(popup_menu_about_to_pop_up)
 		file_system_dock_context_menu.id_pressed.disconnect(popup_menu_id_pressed)
 	
@@ -28,6 +32,7 @@ func _exit_tree() -> void:
 
 
 func popup_menu_about_to_pop_up() -> void:
+	
 	var registered_item_index := file_system_dock_context_menu.get_item_index(POPUP_MENU_ID)
 	
 	if registered_item_index != -1:
@@ -35,20 +40,35 @@ func popup_menu_about_to_pop_up() -> void:
 	
 	var editor_theme := EditorInterface.get_editor_theme()
 	var open_in_icon := editor_theme.get_icon("CodeEdit", "EditorIcons")
-	file_system_dock_context_menu\
-	.add_icon_item(open_in_icon, "Open in external editor", POPUP_MENU_ID)
+	
+	file_system_dock_context_menu.add_icon_item(
+        open_in_icon,
+        "Open in external editor",
+        POPUP_MENU_ID
+    )
 
 
 func popup_menu_id_pressed(id: int) -> void:
+	
 	if id != POPUP_MENU_ID:
 		return
 	
-	var external_editor_path := EditorInterface\
-	.get_editor_settings()\
-	.get_setting("text_editor/external/exec_path")
-	if external_editor_path == null:
-		print("[OpenExternally] Please ensure setting text_editor/external/exec_path\
-		 is set to the path of your editor in Editor settings.")
+	var external_editor_path: String = EditorInterface\
+		.get_editor_settings()\
+		.get_setting("text_editor/external/exec_path")
 	
-	var location := ProjectSettings.globalize_path(Array(EditorInterface.get_selected_paths()).front())
-	OS.execute(external_editor_path, PackedStringArray([location]))
+	if external_editor_path == null:
+		print(
+			"[OpenInEditor] Please ensure setting text_editor/external/exec_path \
+			is set to the path of your editor in Editor settings."
+		)
+		return
+	
+	var selected_paths: PackedStringArray = EditorInterface.get_selected_paths()
+	
+	if selected_paths.is_empty():
+		return
+	
+	var target_path: String = ProjectSettings.globalize_path(selected_paths[0])
+	
+	OS.execute(external_editor_path, PackedStringArray([ target_path ]))
